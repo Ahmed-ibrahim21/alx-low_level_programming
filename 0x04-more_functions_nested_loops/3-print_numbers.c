8427337d1d317d66db495524a81b3059038b495a
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-23 06:49:08 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
+#include "main.h"
 /**
  * print_numbers -a function that prints the numbers, from 0 to 9
  */
-#include "main.h"
 void print_numbers(void)
 {
 for (int i = 0 ; i < 10 ; i++)
 {
-_putchar(i);
+int j = _putchar(i);
 }
 }
