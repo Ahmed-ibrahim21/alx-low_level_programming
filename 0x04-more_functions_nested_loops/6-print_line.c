bc802e4399b40cb80030cd7baf7a4d802b3f8e06
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-23 15:32:20 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line -a function that printsa line with n length
+ * @n : the length of the line
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+_putchar(95);
+}
+}
