eda8e37cb6decfd724c019ce179f2f547657c809
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 07:08:32 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _puts -prints a string
+ * @str : string to print
+ */
+void puts_half(char *str)
+{
+int len = 0;
+int counter;
+int init;
+char *p = str;
+while (*p != '\0')
+{
+len++;
+p++;
+}
+if(len % 2 == 0)
+init = len / 2;
+else
+init = (len - 1) / 2;
+for(counter = init ;counter <= len ; counter++)
+{
+_putchar(str[counter]);
+}
+_putchar('\n');
+}
