c632ffc572f41804a422edb790c3642252dd300e
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 07:59:14 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,19 +5,20 @@
  */
 void puts_half(char *str)
 {
-int len = 1;
-int counter;
-int init;
-char *p = str;
-while (*p != '\0')
-{
+int i;
+int len = 0;
+for (i = 0 ; str[i] != '\0' ; i++)
 len++;
-p++;
-}
-init = len / 2;
-for (counter = init ; counter <= len ; counter++)
+if (len % 2 == 0)
 {
-_putchar(str[counter]);
-}
+for (i = len / 2 ; str[i] != '\0' ; i++)
+_putchar(str[i]);
 _putchar('\n');
+}
+else if (len % 2 != 0)
+{
+for (i = (len - 1) / 2 ; str[i] != '\0' ; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
 }
