f17fdaea3872af0dd6ecea517988ebaede3aa70f
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 08:04:18 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * print_array - a function to print an array
+ * @a: array name
+ * @n: number of elements in the array
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+printf("%d, ",a[i]);
+}
+if (i == (n - 1))
+{
+printf("%d", a[n-1]);
+}
+printf("\n");
+}
