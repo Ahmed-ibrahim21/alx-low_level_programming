37a2daab97f63930494cabb59ce88712f976310a
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 04:08:18 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * reset_to_98 -resets the vlaue of a pointer to 98
+ * @n : pointer to alter
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+today i learn more about pointers arrays and strings
