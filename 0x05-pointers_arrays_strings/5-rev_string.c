4ca2009806ccf4de7d3ac117f44b881c65e0725f
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 06:27:51 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,18 +5,16 @@
  */
 void rev_string(char *s)
 {
-int tracker = 0;
-int counter;
-while (*s != '\0')
+char reverse = s[0];
+int counter = 0;
+int i;
+while (s[counter] != '\0')
+counter++;
+for (i = 0 ; i < counter ; i++)
 {
-tracker++;
-s++;
+counter--;
+reverse = s[i];
+s[i] = s[counter];
+s[counter] = reverse;
 }
-s--;
-for (counter = tracker ; counter > 0 ; counter--)
-{
-_putchar(*s);
-s--;
-}
-_putchar('\n');
 }
