daac49c0919424c7c9f7a5cdd7363249991c515b
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-27 05:09:23 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev -reverses a string
+ * @s : string
+ */
+void print_rev(char *s)
+{
+int tracker = 0;
+int counter ;
+while (*s != '\0')
+{
+tracker++;
+s++;
+}
+s--;
+for (counter = tracker ; counter > 0 ; counter--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
