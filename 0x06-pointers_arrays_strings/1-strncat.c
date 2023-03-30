639be531b6ec254f7b07eeec4a798ef3991e1889
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-30 16:22:55 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int c, c2 = 0;
+int c, c2;
 c = 0;
 while (dest[c])
 c++;
-while (n--)
+for (c2 = 0 ; c2 < n && src[i] != '\0' ; i++)
 {
-dest[c] = src[c2];
-c++;
-c2++;
+dest[c + i] = src[i];
 }
+dest[c + i] = '\0';
 return (dest);
 }
 
