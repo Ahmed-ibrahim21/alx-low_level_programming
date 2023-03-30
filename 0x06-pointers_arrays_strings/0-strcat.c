3d9a344a8ca5cd5855ad876afd62c7bec2ed7bba
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-03-29 17:08:56 -0700

    task 0 retry

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,13 @@ include "main.h"
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-while (src[i] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
+int c, c2;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0 ; src[c2] ; c2++)
+dest[c++] = src[c2];
 return (dest);
 }
+
 
