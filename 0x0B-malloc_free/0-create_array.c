eecd5a3770355b8bc0598f1011820b8d937de6bc
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-04-12 10:45:04 -0700

    adding files + task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *create array - creats an array of characters
+ * @size : size of array
+ * @c : char intialized
+ * Return: pointer or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+char *arr = malloc(size);
+if (size == 0 || arr == 0)
+return 0;
+while(size--)
+arr[size] = c;
+return arr;
+}
+
+
