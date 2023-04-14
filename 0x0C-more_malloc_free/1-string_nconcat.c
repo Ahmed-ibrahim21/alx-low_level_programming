e66ffe2ee0c2eeb916a64bceda643363b132d164
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-04-14 16:39:05 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,34 +15,23 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int size1 = 0, i, size2 = 0, j;
-char *p, *test1;
+unsigned int size1, i, size2, j;
+char *p;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-test1 = s1;
-while (*test1 != '\0')
-{
-test1++;
-size1++;
-}
-test1 = s2;
-while (*test1 != '\0')
-{
-test1++;
-size2++;
-}
-p = malloc((size1 + size2) *sizeof(char) + 1);
+for (size1 = 0; s1[size1] != '\0'; size1++)
+;
+for (size2 = 0; s2[size2] != '\0'; size2++)
+;
+p = malloc(size1 + n + 1);
 if (p == NULL)
 return (NULL);
-for (i = 0; i < size1; i++)
+for (i = 0; s1[i] != '\0'; i++)
 p[i] = s1[i];
-if (n > 0)
-{
 for (j = 0; j < n; j++, i++)
 p[i] = s2[j];
-}
 p[i] = '\0';
 return (p);
 }
