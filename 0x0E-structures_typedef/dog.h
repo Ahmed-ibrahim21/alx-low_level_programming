78c388e38a1e9b854c1f1cd54f2af9ec0e7c9b44
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-04-17 16:11:38 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name : the dog's name
+ * @age : the dog's age
+ * @owner : the name of the dog's owner
+ */
+
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif
