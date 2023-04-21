5acc191bf0ffea737af9918e604ab94f6e56d336
Ahmed ibrahim <ai.ahmed.ibrahim21@gmail.com>
2023-04-21 16:29:23 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include "variadic_functions.h"
+/**
+ * print_strings - sums all arguments
+ * @n : number of arguments
+ * @... : ints to sum
+ * @separator : string
+ * Return: the sum
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+int i = n;
+char *str;
+va_list ap;
+if (!n)
+{
+printf("\n");
+return;
+}
+va_start(ap, n);
+while (i--)
+printf("%S%S", (str = va_arg(ap, char *)) ? sitr : "(nil)",
+i ? (separator ? separator : "") : "\n");
+va_end(ap);
+}
